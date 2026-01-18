ec817be158b65de76afef3e111ef89466df5545d
Sankajith D. Jinasena <sankajithdjinasena@gmail.com>
2026-01-18 22:08:34 +0530

    bat file update

--- Daily Expenses Tracker.bat
+++ Daily Expenses Tracker.bat
@@ -1,6 +1,10 @@
 @echo off
 cd /d E:\Project\Python_Mini_Project\Daily_expense_tracker
 
+:: Install reportlab if it's missing
+"C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe" -m pip install reportlab
+
+:: Run the application
 "C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe" app.py
 
 pause
